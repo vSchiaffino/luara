943c5e43d8ec6b502da52ee2390f7687d3d9eaa1
Valentin Schiaffino <valenschg@gmail.com>
2023-01-20 11:48:46 -0300

    tailwind config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
 export default function Index() {
-  return (
-    <div>
-      <h1>My page</h1>
-      <p>Welcome to my page!</p>
-    </div>
-  )
+  return <h1 className="text-2xl font-bold underline">Hello world!</h1>
 }
