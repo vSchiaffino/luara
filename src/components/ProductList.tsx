2449c3e41660577b125b9f3e7147dcfd05084f67
Valentin Schiaffino <valenschg@gmail.com>
2023-01-20 17:27:50 -0300

    responsive s*

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -4,7 +4,7 @@ import { Product } from './Product'
 
 export const ProductList = ({ products }: { products: IProduct[] }) => {
   return (
-    <div className="grid gap-5 py-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 ">
+    <div className="grid gap-5 py-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 mx-14">
       {products.map((product) => (
         <Product key={product.id} product={product} />
       ))}
